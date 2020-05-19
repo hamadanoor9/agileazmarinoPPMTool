14ff4a41b11e6ad6564032a3fad3a021abedd925
Mohamed Ahmed <mohamed.nurhussien@gmail.com>
2020-05-19 19:11:21 -0400

    Displays list of projects and creates update project component

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import Projectitem from "./Project/Projectitem";
 import CreateProjectButton from "./Project/CreateProjectButton";
-
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectActions";
+import PropTypes from "prop-types";
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
   render() {
+    const { projects } = this.props.project;
     return (
       <div className="projects">
         <div className="container">
@@ -14,8 +20,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-
-              <Projectitem />
+              {projects.map(project => (
+                <Projectitem key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
@@ -24,4 +31,14 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired
+};
+const mapStateTpProps = state => ({
+  project: state.project
+});
+export default connect(
+  mapStateTpProps,
+  { getProjects }
+)(Dashboard);
